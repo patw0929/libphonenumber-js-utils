bc9a727c961c8eba06a305eeb89d46dc8faa5172
npham <13-npham@users.noreply.gitlab.maiia.io>
2023-11-23 11:48:58 +0100

    update libphonenumber and build script

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -7,8 +7,5 @@ cp src/libphonenumber.js libphonenumber/javascript/i18n/phonenumbers/demo.js
 cp src/PhoneNumberMetadata.xml libphonenumber/resources/PhoneNumberMetadata.xml
 cp src/PhoneNumberMetadataForTesting.xml libphonenumber/resources/PhoneNumberMetadataForTesting.xml
 
-ant -f libphonenumber/java/build.xml build-js-metadata
-ant -f libphonenumber/javascript/build.xml compile-demo
-
 mkdir dist
-node src/minify.js
+node src/compiler.js
